bd7f4b38f1f5a9ab59d4b15945ab2abb3cd03603
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-03 00:47:13 +0330

    configuration enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+it("renders without crashing",()=>{
+  
+})
