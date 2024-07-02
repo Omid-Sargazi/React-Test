347a21abc01c3a25d1b3ff284d5ed5b85c69d60c
Omid-Saegazi <om.sargazi@gmail.com>
2024-07-03 00:53:30 +0330

    debug test with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Enzyme from 'enzyme';
+import Enzyme,{shallow} from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
 import App from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it("renders without crashing",()=>{
-  
+  const wrapper = shallow(<App/>)
+  console.log(wrapper.debug());
 })
